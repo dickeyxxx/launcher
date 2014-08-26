170e32305779974bd58246f6b9146e0a2d9986e1
dickeyxxx <jeff@dickeyxxx.com>
2014-08-26 15:23:36 -0700

    gox

--- run.go
+++ run.go
@@ -3,9 +3,19 @@ package main
 import (
 	"os"
 	"os/exec"
+	"runtime"
+	"syscall"
 )
 
 func run(path string, args []string) error {
+	if runtime.GOOS == "windows" {
+		return runWindows(path, args)
+	}
+	env := os.Environ()
+	return syscall.Exec(path, args, env)
+}
+
+func runWindows(path string, args []string) error {
 	cmd := exec.Command(path, args[1:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
--- util.go
+++ util.go
@@ -1,6 +1,10 @@
 package main
 
-import "os/user"
+import (
+	"os"
+	"os/user"
+	"runtime"
+)
 
 func homeDir() string {
 	user, err := user.Current()
@@ -12,4 +16,21 @@ func must(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func fileExists(path string) (bool, error) {
+	var err error
+	if runtime.GOOS == "windows" {
+		// Windows doesn't seem to like using os.Stat
+		_, err = os.Open(path)
+	} else {
+		_, err = os.Stat(path)
+	}
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
 }
