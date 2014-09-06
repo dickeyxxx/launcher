b315cf8244a09036629928d13b37a54613884f28
dickeyxxx <jeff@dickeyxxx.com>
2014-09-05 21:15:24 -0700

    moved run.go into main.go

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"os"
+	"os/exec"
+	"runtime"
+	"syscall"
+)
 
 func main() {
 	if isUpdateCheckNeeded() {
@@ -11,4 +16,20 @@ func main() {
 	}
 	err := run(os.Args)
 	must(err)
+}
+
+func run(args []string) error {
+	if runtime.GOOS == "windows" {
+		return runWindows(args)
+	}
+	env := os.Environ()
+	return syscall.Exec(hkPath(), args, env)
+}
+
+func runWindows(args []string) error {
+	cmd := exec.Command(hkPath(), args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
